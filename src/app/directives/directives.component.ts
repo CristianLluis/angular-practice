273a685d64500cfd1be3c4655a32694a2abcf12a
Cristian Lluis <cristian@lluis.ch>
2022-01-28 20:39:38 +0100

    new component for practicing directives

--- src/app/directives/directives.component.ts
+++ src/app/directives/directives.component.ts
@@ -7,7 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectivesComponent implements OnInit {
 
+  paragraphVisible: Boolean = false
+  logs: String[] = []
+
   constructor() { }
+
+  toggleVisibility = () => {
+    this.paragraphVisible = ! this.paragraphVisible
+    this.logClick();
+  }
+
+  logClick = () => this.logs.push(this.getTimeStamp() + ": Visibility has been toggled to: " + this.paragraphVisible)  
+
+  getTimeStamp = () => {
+    let date = new Date()
+    let day = date.getDate()
+    let month = date.getMonth()
+    let year = date.getFullYear()
+    let hour = date.getHours()
+    let minute = date.getMinutes()
+    let sec = date.getSeconds()
+    return `${day}/${month}/${year} - ${hour}:${minute}:${sec}`
+  }
+
+  getBackgroundColor = (i: number) => i >= 4 ? 'cadetblue' : 'transparent'
+
+  getColor = (i: number) => i >= 4
 
   ngOnInit(): void {
   }
