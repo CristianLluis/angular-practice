9851a17cd2c142c558e8be7b3776dff59513e650
Cristian Lluis <cristian@lluis.ch>
2022-01-28 23:08:03 +0100

    displays some dummy recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Bratwurst mit Spätzle', 'Bratwurst bruacht keine Beschreibung', 'https://wips.plug.it/cips/buonissimo.org/cms/2012/01/bratwurst-con-spatzle.jpg?w=541&a=c&h=309'),
+    new Recipe('Spaguetti Carbonara', 'Beste Spaguetti ever!', 'https://upload.wikimedia.org/wikipedia/commons/3/33/Espaguetis_carbonara.jpg')
+  ];
 
   constructor() { }
 
